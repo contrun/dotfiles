76c0a22674160dacbc2159bce81f7f0467c7b2c4
YI <uuuuuu@protonmail.com>
2022-08-17 22:21:49 +0800

    neovim: do not ensure treesitter
    some machines can not compile treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -16,5 +16,5 @@ require('nvim-treesitter.configs').setup({
     disable = { 'org' }, -- Remove this to use TS highlighter for some of the highlights (Experimental)
     additional_vim_regex_highlighting = { 'org' } -- Required since TS highlighter doesn't support all syntax features (conceal)
   },
-  ensure_installed = "all"
+  -- ensure_installed = "all"
 })
